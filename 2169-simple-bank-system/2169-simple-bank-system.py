db1e0e2c57cee1d0cf57dc2696d0159184862688
Fulati Aizihaer <82471145+fulati@users.noreply.github.com>
2025-10-14 20:25:08 -0700

    Time: 35 ms (54.11%), Space: 47.7 MB (82.22%) - LeetHub

--- 2169-simple-bank-system/2169-simple-bank-system.py
+++ 2169-simple-bank-system/2169-simple-bank-system.py
@@ -1,45 +1,33 @@
 class Bank:
-    def __init__(self, balance: List[int]):
-        # balance[i] stores the money in account (i+1)
-        self.bal = balance
-        self.n = len(balance)
 
-    def transfer(self, account1: int, account2: int, money: int) -> bool:
-        # account numbers are 1-indexed (1 to n)
-        if account1 < 1 or account1 > self.n or account2 < 1 or account2 > self.n:
+    def __init__(self, balance):
+        self.balance = balance
+        self.n = len(balance) 
+    
+    def transfer(self, acc1, acc2, money):
+        if acc1 < 1 or acc1 > self.n or acc2 < 1 or acc2 > self.n:
             return False
-        # convert to 0-index for internal list access
-        i, j = account1 - 1, account2 - 1
-        # check enough money
-        if self.bal[i] < money:
-            return False
-        # perform transfer
-        self.bal[i] -= money
-        self.bal[j] += money
-        return True
-
-    def deposit(self, account: int, money: int) -> bool:
-        # check account validity
-        if account < 1 or account > self.n:
-            return False
-        self.bal[account - 1] += money
-        return True
-
-    def withdraw(self, account: int, money: int) -> bool:
-        if account < 1 or account > self.n:
-            return False
-        idx = account - 1
-        if self.bal[idx] < money:
-            return False
-        self.bal[idx] -= money
-        return True
-
-
         
+        if self.balance[acc1 - 1] < money:
+            return False
+        
+        self.balance[acc1 - 1] -= money
+        self.balance[acc2 - 1] += money
+        return True
+    
+    def deposit(self, acc, money):
+        if acc < 1 or acc > self.n:
+            return False
 
+        self.balance[acc - 1] += money
+        return True
 
-# Your Bank object will be instantiated and called as such:
-# obj = Bank(balance)
-# param_1 = obj.transfer(account1,account2,money)
-# param_2 = obj.deposit(account,money)
-# param_3 = obj.withdraw(account,money)
+    def withdraw(self, acc, money):
+        if acc < 1 or acc > self.n:
+            return False
+        
+        if self.balance[acc - 1] < money:
+            return False
+        
+        self.balance[acc - 1] -= money
+        return True
